1a73800489beb6f8152f68d430b5e9d774dea961
PellicerAngel <151621521+PellicerAngel@users.noreply.github.com>
2025-02-08 19:28:49 +0100

    Update test_resta.py

--- src/proves/test_src/test_resta.py
+++ src/proves/test_src/test_resta.py
@@ -19,17 +19,3 @@ def test_suma_positivos_negativos():
 def test_suma_nagativo():
     assert resta.resta_enteros(-5,-5) == 0
 
-
-def test_suma_excepcion_sumando1():
-    with pytest.raises(TypeError):
-        resta.resta_enteros(5.0,5) == 0
-
-
-def test_suma_excepcion_sumando2():
-   with pytest.raises(TypeError):
-       resta.resta_enteros(5.0,'a') == 3
-
-
-def test_suma_excepcion_sumandos():
-   with pytest.raises(TypeError):
-       resta.resta_enteros(True,'a') == 8
