914c2b4efc058a1068792b52cef4cd75ee0eb9d9
PellicerAngel <151621521+PellicerAngel@users.noreply.github.com>
2025-02-08 18:58:14 +0100

    Delete resta.py

--- resta.py
+++
@@ -1,6 +0,0 @@
-def resta_enteros(sumando1, sumando2):
-    if not isinstance(sumando1, int) or not isinstance(sumando2, int):
-        raise TypeError('Proporciona solo enteros')
-    else:
-        resultado = sumando1 - sumando2
-    return resultado
