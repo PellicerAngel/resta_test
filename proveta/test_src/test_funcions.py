2e7cf93b4678b62588a163329f43fe26ce51e338
PellicerAngel <151621521+PellicerAngel@users.noreply.github.com>
2025-02-08 20:55:00 +0100

    Add files via upload

---
+++ proveta/test_src/test_funcions.py
@@ -0,0 +1,35 @@
+import pytest
+
+
+import resta
+
+
+def test_suma_cero():
+    assert resta.resta_enteros(0,0) == 0
+
+
+def test_suma_positivos():
+    assert resta.resta_enteros(5,5) == 0
+
+
+def test_suma_positivos_negativos():
+    assert resta.resta_enteros(-5,5) == -10
+
+
+def test_suma_nagativo():
+    assert resta.resta_enteros(-5,-5) == 0
+
+
+def test_suma_excepcion_sumando1():
+    with pytest.raises(TypeError):
+        resta.resta_enteros(5.0,5) == 0
+
+
+def test_suma_excepcion_sumando2():
+   with pytest.raises(TypeError):
+       resta.resta_enteros(5.0,'a') == 3
+
+
+def test_suma_excepcion_sumandos():
+   with pytest.raises(TypeError):
+       resta.resta_enteros(True,'a') == 8
